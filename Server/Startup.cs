5b98ddc5eb090886f1bc09c007ddf58230767c68
Fabio Almeida <almeida.fb@gmail.com>
2024-02-27 22:27:50 -0300

    Open Telemetry

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Hosting;
@@ -6,13 +7,26 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Otm.Server.ContextConfig;
+using Otm.Server.OpenTelemetry;
 using Otm.Server.Services;
 
 namespace Otm.Server
 {
     public class Startup
     {
+        private readonly IConfiguration _appConfiguration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        private readonly bool _openTelemetryEnabled = false;
+        private readonly string _jaegerHost = string.Empty;
+        private readonly int _jaegerPort = 0;
+        
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -22,6 +36,19 @@ namespace Otm.Server
             DeviceService = new DeviceService();
             LogsService = new LogsService();
             TransactionService = new TransactionService();
+            
+            bool.TryParse(_appConfiguration["OpenTelemetry:IsEnabled"], out _openTelemetryEnabled);
+
+            if (_openTelemetryEnabled)
+            {
+                _jaegerHost = _appConfiguration["Jaeger:Host"];
+                
+                if(!int.TryParse(_appConfiguration["Jaeger:Port"], out _jaegerPort))
+                {
+                    throw new Exception("Invalid Jaeger port");
+                }
+                
+            }
         }
 
         public IConfiguration Configuration { get; }
@@ -52,6 +79,53 @@ namespace Otm.Server
 
             services.AddSingleton<OtmWorkerService>();
             services.AddHostedService(provider => provider.GetService<OtmWorkerService>());
+            
+            if (_openTelemetryEnabled)
+            {
+                var resource = ResourceBuilder.CreateDefault().AddService(
+                    serviceName: TelemetryConstants.ServiceName, 
+                    serviceNamespace: TelemetryConstants.ServiceNameSpace,
+                    serviceVersion: TelemetryConstants.ServiceVersion
+                    );
+
+                services.AddOpenTelemetry()
+                    .WithTracing(b =>
+                        {
+                            b.AddSource(TelemetryConstants.ServiceName);
+                            
+                            // decorate our service name so we can find it when we look inside Jaeger
+                            b.SetResourceBuilder(resource);
+
+                            // receive traces from built-in sources
+                            b.AddHttpClientInstrumentation();
+                            b.AddAspNetCoreInstrumentation();
+                            b.AddSqlClientInstrumentation();
+
+                            b.AddJaegerExporter(e =>
+                            {
+                                e.AgentHost = _jaegerHost;
+                                e.AgentPort = Convert.ToInt32(_appConfiguration["Jaeger:Port"]);
+                            });
+                        }
+                    )
+                    .WithMetrics(b =>
+                    {
+                        // add prometheus exporter
+                        b.AddPrometheusExporter();
+
+                        // receive metrics from our own custom sources
+                        b.AddMeter(TelemetryConstants.MyAppSource);
+
+                        // decorate our service name so we can find it when we look inside Prometheus
+                        b.SetResourceBuilder(resource);
+
+                        // receive metrics from built-in sources
+                        b.AddHttpClientInstrumentation();
+                        b.AddAspNetCoreInstrumentation();
+                    });
+
+            }
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -81,6 +155,11 @@ namespace Otm.Server
                 endpoints.MapControllers();
             });
             
+            if (_openTelemetryEnabled)
+            {
+                // add the /metrics endpoint which will be scraped by Prometheus
+                app.UseOpenTelemetryPrometheusScrapingEndpoint();
+            }
 
             // For the wwwroot folder
             app.UseDefaultFiles();
